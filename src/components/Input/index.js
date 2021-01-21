2f4a5dff3a2600474d82cdfee34382976690db59
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2021-01-21 01:23:02 +0000

    added a input functionality to the login forms

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -4,16 +4,16 @@ import { FaUser } from "react-icons/fa";
 function Input({ icon, type, placeholder }) {
   return (
     // <input type="text" className="form-control my-3" placeholder="Full Name" />
-    <div class="input-group mb-3">
+    <div className="input-group mb-3">
       <span
-        class="icon-color input-group-text bg-white border-end-0"
+        className="icon-color input-group-text bg-white border-end-0"
         id="basic-addon1"
       >
         {icon}
       </span>
       <input
         type={type}
-        class="form-control border-start-0 p-2"
+        className="form-control border-start-0 p-2"
         placeholder={placeholder}
       />
     </div>
--- src/pages/auth/Login/index.js
+++ src/pages/auth/Login/index.js
@@ -3,31 +3,55 @@ import { loginImage } from "../../../assets/images";
 import { Input } from "../../../components/Input";
 import { FaEnvelope, FaLock } from "react-icons/fa";
 import { Button } from "../../../components/Button";
+import { useState } from "react";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setUser({ [e.target.name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
   return (
     <main className="vh-100 container-fluid login-section">
       <section className="row h-100">
         <div className="col-6 d-flex justify-content-center align-items-center">
           <div className="card shadow w-c">
-            <form>
+            <form onSubmit={handleSubmit}>
               <h2 className="font-weight-bold text-center">Login</h2>
               <hr className="mt-n5" />
               <Input
                 type="email"
                 icon={<FaEnvelope />}
                 placeholder="Enter Email"
+                onChange={handleChange}
+                value={user.email}
+                name="email"
               />
               <Input
                 type="password"
                 icon={<FaLock />}
                 placeholder="Enter Password"
+                onChange={handleChange}
+                value={user.password}
+                name="password"
               />
 
               <Button className="btn btn-primary w-100" text="Login" />
 
               <hr />
-              <p className="signup-p">New Member? Sign Up</p>
+              <p className="signup-p">
+                New Member? <a href="#">Sign Up</a>
+              </p>
             </form>
           </div>
         </div>
