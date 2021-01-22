bbe116e9b3be8bab95baab2cb2397ccf42af4b6f
Peter Okerulu <oduchep@gmail.com>
2021-01-22 13:23:45 +0100

    ....

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React from "react";
-import VerifyEmail from "../pages/auth/VerifyEmail/index";
-// import { SignUp } from "../pages/auth/SignUp";
+
+import { SignUp } from "../pages/auth/SignUp";
 import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
-			<VerifyEmail />
-			{/* <SignUp /> */}
+			<SignUp />
 		</div>
 	);
 }
