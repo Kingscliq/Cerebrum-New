8b33220d400a64ff6768073ced35018ab94a6806
MayorErnest <eobumma@gmail.com>
2021-01-25 14:06:07 +0100

    Add DashboardHeader component to widgets folder

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,7 +3,9 @@ import verifyImage from "./verifyimage.jpg";
 import loginImg from "./loginimg.jpg";
 import loaderImage from "./loaderimage.gif";
 import forgotPasswordImg from "./forgotpasswordimage.jpg";
+import logo from "./logo.png";
 import logo2 from "./logo2.png";
+import dashAvater from "./dashavatar.png";
 
 export {
   signupImage,
@@ -12,4 +14,6 @@ export {
   loaderImage,
   loginImg,
   logo2,
+  logo,
+  dashAvater,
 };
--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./components/App";
-import { Footer } from "./widgets/Footer";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Footer />
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
