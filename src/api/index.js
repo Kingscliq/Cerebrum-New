7d36b030b12dcbade0aad52f8c8ad1f8bceebb04
Peter Okerulu <oduchep@gmail.com>
2021-01-26 11:36:19 +0100

    some mild changes to the email verification page

--- src/api/index.js
+++ src/api/index.js
@@ -1,43 +1,64 @@
 import axios from "axios";
 
 export const signUpReg = (e, state, setLoadState) => {
-  e.preventDefault();
+	e.preventDefault();
 
-  setLoadState(true);
-  let currentState = state;
-  console.log(currentState);
+	setLoadState(true);
+	let currentState = state;
+	console.log(currentState);
 
-  axios
-    .post(`https://cerebrum-v1.herokuapp.com/api/auth/sign-up`, currentState)
-    .then((res) => {
-      console.log(res.data);
-      setLoadState(false);
-    })
-    .catch(() => {
-      console.log("Error Occured");
-      setLoadState(false);
-    });
+	axios
+		.post(`https://cerebrum-v1.herokuapp.com/api/auth/sign-up`, currentState)
+		.then((res) => {
+			console.log(res.data);
+			setLoadState(false);
+
+			const url = `/verifyemail?email=${currentState.email}&name=${currentState.firstName}`;
+
+			window.location.assign(url);
+		})
+		.catch(() => {
+			console.log("Error Occured");
+			setLoadState(false);
+		});
 };
 
 /// Login Api
 export const signIn = (e, user, setUser, setLoadState) => {
-  e.preventDefault();
-  setLoadState(true);
-  const data = {
-    email: user.email,
-    password: user.password,
-  };
+	e.preventDefault();
+	setLoadState(true);
+	const data = {
+		email: user.email,
+		password: user.password,
+	};
 
-  axios
-    .post("https://new-cerebrum.herokuapp.com/api/auth/sign-in", data)
-    .then((res) => {
-      console.log(res.data);
-      const token = res.data.token;
-      localStorage.setItem("token", token);
-      setLoadState(false);
-    })
-    .catch((err) => {
-      console.log("there is an error logging in", err);
-      setLoadState(false);
-    });
+	axios
+		.post("https://new-cerebrum.herokuapp.com/api/auth/sign-in", data)
+		.then((res) => {
+			console.log(res.data);
+			const token = res.data.token;
+			localStorage.setItem("token", token);
+			setLoadState(false);
+		})
+		.catch((err) => {
+			console.log("there is an error logging in", err);
+			setLoadState(false);
+		});
+};
+
+//email verification
+export const emailVerification = (e, email, setEmail) => {
+	e.preventDefault();
+	const data = {
+		email: email,
+	};
+
+	axios
+		.post(`https://new-cerebrum.herokuapp.com/api/auth/request-email-verification?email=${data.email}`, data)
+		.then((res) => {
+			console.log(res.data);
+		})
+		.catch((err) => {
+			console.log("there is an error sending verification email", err);
+		});
 };
--- src/pages/auth/SignUp/index.js
+++ src/pages/auth/SignUp/index.js
@@ -9,128 +9,116 @@ import { signUpReg } from "../../../api";
 import { Loader } from "../../../components/Loader";
 
 function SignUp() {
-  const [signUpData, setSignUpData] = useState({
-    firstname: "",
-    lastname: "",
-    email: "",
-    password: "",
-  });
+	const [signUpData, setSignUpData] = useState({
+		firstName: "",
+		lastName: "",
+		email: "",
+		password: "",
+		role: "tutor",
+	});
 
-  const [loading, setLoading] = useState(false);
-  const [numToogle, setNumToogle] = useState(1);
+	const [loading, setLoading] = useState(false);
+	const [numToogle, setNumToogle] = useState(1);
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    let targetText = e.target.innerText;
-    targetText === "Tutor" ? setNumToogle(0) : setNumToogle(1);
-    console.log(targetText);
-  };
+	const handleClick = (e) => {
+		e.preventDefault();
+		let targetText = e.target.innerText;
+		targetText === "Tutor" ? setNumToogle(0) : setNumToogle(1);
+		console.log(targetText);
+	};
 
-  const handleSubmit = (e) => {
-    signUpReg(e, signUpData, setLoading);
-  };
+	const handleSubmit = (e) => {
+		signUpReg(e, signUpData, setLoading);
+	};
 
-  const handleChange = (e) => {
-    const value = e.target.value;
-    setSignUpData({ ...signUpData, [e.target.name]: value });
-  };
+	const handleChange = (e) => {
+		const value = e.target.value;
+		setSignUpData({ ...signUpData, [e.target.name]: value });
+	};
 
-  return (
-    <main className="container-fluid vh-100 signup-section">
-      <section className="row h-100">
-        <div className="col-7 d-flex justify-content-center align-items-center">
-          <div className="card shadow w-c">
-            <form onSubmit={handleSubmit}>
-              <h2 className="fs-5">Create Account</h2>
-              <hr className="mt-n5" />
-              <div className="btn-container">
-                <Button
-                  className={`btn btn-primary ${
-                    numToogle == 0 ? "active" : ""
-                  } w-50`}
-                  text="Learner"
-                  handleClick={handleClick}
-                  disabled={loading}
-                />
-                <Button
-                  className={`btn btn-primary ${
-                    numToogle == 1 ? "active" : ""
-                  } w-50`}
-                  text="Tutor"
-                  handleClick={handleClick}
-                  disabled={loading}
-                />
-              </div>
-              <Input
-                type="text"
-                icon={<FaUser />}
-                placeholder="First Name"
-                name="firstname"
-                onChange={handleChange}
-                value={signUpData.firstname || ""}
-                disabled={loading}
-              />
-              <Input
-                type="text"
-                icon={<FaUser />}
-                placeholder="Last Name"
-                name="lastname"
-                onChange={handleChange}
-                value={signUpData.lastname || ""}
-                disabled={loading}
-              />
-              <Input
-                type="email"
-                icon={<FaEnvelope />}
-                placeholder="Email"
-                name="email"
-                onChange={handleChange}
-                value={signUpData.email || ""}
-                disabled={loading}
-              />
-              <Input
-                type="password"
-                icon={<FaLock />}
-                placeholder="Password"
-                name="password"
-                onChange={handleChange}
-                value={signUpData.password || ""}
-                disabled={loading}
-              />
-              <p className="signup-p">
-                Use 8 or more characters with a mix of letters, number & symbols
-              </p>
-              <Button
-                className="btn btn-primary w-100"
-                text={"Sign Up"}
-                loadingIcon={loading && <Loader />}
-                disabled={loading}
-              />
-              <p className="signup-p pt-2">
-                By signing up, you agree to our <span>Terms of Use</span> &
-                <span> Privacy Policy.</span>
-              </p>
-              <hr />
-              <p className="signup-p">
-                Already have an account?{" "}
-                <span>
-                  <Link to="/login">Log In</Link>
-                </span>
-              </p>
-            </form>
-          </div>
-        </div>
-        <div
-          className="col-5 h-100 signup-image"
-          style={{
-            background: `url(${signupImage})`,
-            backgroundRepeat: `no-repeat`,
-            backgroundSize: `cover`,
-          }}
-        ></div>
-      </section>
-    </main>
-  );
+	return (
+		<main className="container-fluid vh-100 signup-section">
+			<section className="row h-100">
+				<div className="col-7 d-flex justify-content-center align-items-center">
+					<div className="card shadow w-c">
+						<form onSubmit={handleSubmit}>
+							<h2 className="fs-5">Create Account</h2>
+							<hr className="mt-n5" />
+							<div className="btn-container">
+								<Button
+									className={`btn btn-primary ${numToogle === 0 ? "active" : ""} w-50`}
+									text="Learner"
+									handleClick={handleClick}
+									disabled={loading}
+								/>
+								<Button
+									className={`btn btn-primary ${numToogle === 1 ? "active" : ""} w-50`}
+									text="Tutor"
+									handleClick={handleClick}
+									disabled={loading}
+								/>
+							</div>
+							<Input
+								type="text"
+								icon={<FaUser />}
+								placeholder="First Name"
+								name="firstName"
+								onChange={handleChange}
+								value={signUpData.firstName || ""}
+								disabled={loading}
+							/>
+							<Input
+								type="text"
+								icon={<FaUser />}
+								placeholder="Last Name"
+								name="lastName"
+								onChange={handleChange}
+								value={signUpData.lastName || ""}
+								disabled={loading}
+							/>
+							<Input
+								type="email"
+								icon={<FaEnvelope />}
+								placeholder="Email"
+								name="email"
+								onChange={handleChange}
+								value={signUpData.email || ""}
+								disabled={loading}
+							/>
+							<Input
+								type="password"
+								icon={<FaLock />}
+								placeholder="Password"
+								name="password"
+								onChange={handleChange}
+								value={signUpData.password || ""}
+								disabled={loading}
+							/>
+							<p className="signup-p">Use 8 or more characters with a mix of letters, number & symbols</p>
+							<Button className="btn btn-primary w-100" text={"Sign Up"} loadingIcon={loading && <Loader />} disabled={loading} />
+							<p className="signup-p pt-2">
+								By signing up, you agree to our <span>Terms of Use</span> &<span> Privacy Policy.</span>
+							</p>
+							<hr />
+							<p className="signup-p">
+								Already have an account?{" "}
+								<span>
+									<Link to="/login">Log In</Link>
+								</span>
+							</p>
+						</form>
+					</div>
+				</div>
+				<div
+					className="col-5 h-100 signup-image"
+					style={{
+						background: `url(${signupImage})`,
+						backgroundRepeat: `no-repeat`,
+						backgroundSize: `cover`,
+					}}></div>
+			</section>
+		</main>
+	);
 }
 
 export { SignUp };
