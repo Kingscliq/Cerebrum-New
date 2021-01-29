2f2ce16aef6738082dd72b0be8ad2373e46bd87a
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2021-01-29 04:01:41 -0800

    fixining of the dashboard Avatar

---
+++ src/pages/app/Logout/index.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import React, { useEffect } from "react";
+import { Redirect } from "react-router-dom";
+
+const Logout = () => {
+  useEffect(() => {
+    localStorage.clear();
+  });
+  return <Redirect to='/login' />;
+};
+
+export { Logout };
