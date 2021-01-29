bdd1c713c3feaed28f30152473a9f4b1b7044915
Peter Okerulu <oduchep@gmail.com>
2021-01-29 06:17:52 +0100

    merged the files I pulled successfully

--- src/api/index.js
+++ src/api/index.js
@@ -3,22 +3,22 @@
 import axios from "axios";
 
 export const signUpReg = (e, state, setLoadState) => {
-  e.preventDefault();
+	e.preventDefault();
 
-  setLoadState(true);
-  let currentState = state;
-  console.log(currentState);
+	setLoadState(true);
+	let currentState = state;
+	console.log(currentState);
 
-  axios
-    .post(`https://cerebrum-v1.herokuapp.com/api/auth/sign-up`, currentState)
-    .then((res) => {
-      console.log(res.data);
-      setLoadState(false);
-    })
-    .catch(() => {
-      console.log("Error Occured");
-      setLoadState(false);
-    });
+	axios
+		.post(`https://cerebrum-v1.herokuapp.com/api/auth/sign-up`, currentState)
+		.then((res) => {
+			console.log(res.data);
+			setLoadState(false);
+		})
+		.catch(() => {
+			console.log("Error Occured");
+			setLoadState(false);
+		});
 };
 
 export const getCategories = (stateFunction) => {
@@ -29,38 +29,37 @@ export const getCategories = (stateFunction) => {
 
 /// Login Api
 export const signIn = (e, user, setUser, setLoadState) => {
-  e.preventDefault();
-  setLoadState(true);
-  const data = {
-    email: user.email,
-    password: user.password,
-  };
+	e.preventDefault();
+	setLoadState(true);
+	const data = {
+		email: user.email,
+		password: user.password,
+	};
 
-  axios
-    .post("https://cerebrum-v1.herokuapp.com/api/auth/sign-in", data)
-    .then((res) => {
-      console.log(res.data);
+	axios
+		.post("https://cerebrum-v1.herokuapp.com/api/auth/sign-in", data)
+		.then((res) => {
+			console.log(res.data);
 
-      const userDetails = JSON.stringify(res.data);
-      localStorage.setItem("userDetails", userDetails);
-      setLoadState(false);
+			const userDetails = JSON.stringify(res.data);
+			localStorage.setItem("userDetails", userDetails);
+			setLoadState(false);
 
-      console.log(userDetails);
-      window.open("/tdashboard", "_self");
-    })
-    .catch((err) => {
-      console.log(err.response.data.message);
-      //   if (
-      //     err.response.data.message ===
-      //     "Email not verified, kindly check your email for verification link"
-      //   ) {
-      //     window.open(`/verifyemail?email=${data.email}`, "_self");
-      //   }
-    });
+			console.log(userDetails);
+			window.open("/tdashboard", "_self");
+		})
+		.catch((err) => {
+			console.log(err.response.data.message);
+			//   if (
+			//     err.response.data.message ===
+			//     "Email not verified, kindly check your email for verification link"
+			//   ) {
+			//     window.open(`/verifyemail?email=${data.email}`, "_self");
+			//   }
+		});
 };
 //Courses Api
 export const getCourses = (courses, setCourses) => {
-<<<<<<< HEAD
 	const data = courses;
 	const course_id = "6012ea1b057ae600157c2902";
 
@@ -73,23 +72,4 @@ export const getCourses = (courses, setCourses) => {
 		.catch((err) => {
 			console.log("There is an error loading files", err.response);
 		});
-=======
-  // e.preventDefault();
-  // setLoadState(true);
-  const data = [courses];
-  // const courseId = 12345;
-
-  axios
-    .get(`https://new-cerebrum.herokuapp.com/api/course/12345`, data)
-    .then((res) => {
-      console.log(res.data);
-      setCourses(data);
-
-      // setLoadState(false);
-    })
-    .catch((err) => {
-      console.log("There is an error loading files", err.response.data);
-      // setLoadState(false);
-    });
->>>>>>> 4dd6ce1c2d8bd94e8b6be2e57a403d860ccdf7fa
 };
--- src/index.js
+++ src/index.js
@@ -6,14 +6,13 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 // import { AddCourses } from "./pages/app/AddCourses";
-import App from "./components/App";
+import App from "../src/components/App";
 import { Dashboard } from "./pages/app/Dashboard";
 import { DashboardHeader } from "./widgets/DashboardHeader";
 import { Footer } from "./widgets/Footer";
 import { EmailVerify } from "./pages/auth/EmailVerify";
 
 ReactDOM.render(
-<<<<<<< HEAD
 	<React.StrictMode>
 		<BrowserRouter>
 			<App />
@@ -21,13 +20,4 @@ ReactDOM.render(
 		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById("root")
-=======
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      {/* <AddCourses /> */}
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
->>>>>>> 4dd6ce1c2d8bd94e8b6be2e57a403d860ccdf7fa
 );
