4751c7ddee92b5163f42fdc86dde2d8638a1b928
MayorErnest <eobumma@gmail.com>
2021-01-29 04:18:14 +0100

    Fix merge conflict

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -8,7 +8,6 @@ import forgotPasswordImg from "./forgotpasswordimage.jpg";
 import logo from "./logo.png";
 import logo2 from "./logo2.png";
 import dashAvater from "./dashavatar.png";
-<<<<<<< HEAD
 import dashboardImg from "./dashboard-img.png";
 import dashImg from "./dashimg.png";
 import ellipsesvg1 from "./ellipsesvg1.svg";
@@ -18,11 +17,9 @@ import Play from "./play.png";
 import ActiveCircle from "./active-circle.png";
 import Circle from "./circle.png";
 import dashboardImage from "./dashboardimage.png";
-=======
 import landingPage1 from "./landingpage1.jpg";
 import landingPage2 from "./landingpage2.png";
 import landingPage3 from "./landingpage3.png";
->>>>>>> feature/authentication
 
 export {
   signupImage,
@@ -33,7 +30,6 @@ export {
   logo2,
   logo,
   dashAvater,
-<<<<<<< HEAD
   dashImg,
   dashboardImg,
   ellipsesvg1,
@@ -41,9 +37,7 @@ export {
   dashboardImage,
   ActiveCircle,
   Circle,
-=======
   landingPage1,
   landingPage2,
   landingPage3,
->>>>>>> feature/authentication
 };
--- src/index.js
+++ src/index.js
@@ -5,17 +5,13 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-<<<<<<< HEAD
 import { AddCourses } from "./pages/app/AddCourses";
-import App from "./components/App";
 import { Dashboard } from "./pages/app/Dashboard";
 import { DashboardHeader } from "./widgets/DashboardHeader";
 import { Footer } from "./widgets/Footer";
 import { EmailVerify } from "./pages/auth/EmailVerify";
-=======
 import App from "./components/App";
 import { LandingPage } from "./pages/app/LandingPage";
->>>>>>> feature/authentication
 
 ReactDOM.render(
   <React.StrictMode>
