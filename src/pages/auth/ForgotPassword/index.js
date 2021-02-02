5469a56640c39cd7ff48f9c1b56fe394949e8a8b
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2021-02-02 11:40:45 +0100

    commit of part of the forgot password functionality before the api call failure

--- src/pages/auth/ForgotPassword/index.js
+++ src/pages/auth/ForgotPassword/index.js
@@ -6,15 +6,32 @@ import { forgotPasswordImg } from "../../../assets/images";
 import { Button } from "../../../components/Button";
 import { Input } from "../../../components/Input";
 import "./ForgotPassword.css";
+import axios from "axios";
 function ForgotPassWord() {
   const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setEmail(e.target.value);
+  };
+
+  const handleForgotPassword = (e) => {
+    e.preventDefault();
+    axios
+      .get(
+        `https://cerebrum-v1.herokuapp.com/api/auth/request-password-reset?email=${email}`
+      )
+      .then((res) => console.log(res))
+      .catch((err) => console.log("error", err.response));
+  };
 
   return (
     <main className='container-fluid vh-100 fpassword-section'>
       <section className='row h-100'>
         <div className='col-7 d-flex justify-content-center align-items-center'>
           <div className='card shadow w-c'>
-            <form>
+            <form onSubmit={handleForgotPassword}>
               <h2 className='fs-5'>Find Your Account</h2>
               <hr className='mt-n5' />
               {error && (
@@ -26,8 +43,18 @@ function ForgotPassWord() {
                 </div>
               )}
 
-              <Input type='text' icon={<FaEnvelope />} placeholder='Email' />
-              <Button className='btn btn-primary w-100' text={"Search"} />
+              <Input
+                type='text'
+                icon={<FaEnvelope />}
+                placeholder='Email'
+                value={email}
+                onChange={handleChange}
+              />
+              <Button
+                className='btn btn-primary w-100'
+                text={"Search"}
+                onClick={handleForgotPassword}
+              />
             </form>
           </div>
         </div>
