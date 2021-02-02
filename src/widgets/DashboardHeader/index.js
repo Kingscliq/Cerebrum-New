7f6a75a945dba9c95f1f5a5fc7ea42a15afd7991
MayorErnest <eobumma@gmail.com>
2021-02-02 11:54:48 +0100

    Add responsiveness to dashboard header and footer

--- src/widgets/DashboardHeader/index.js
+++ src/widgets/DashboardHeader/index.js
@@ -10,26 +10,26 @@ import "./DashboardHeader.css";
 function DashboardHeader() {
   // const dashimage = props.dashimage;
   return (
-    <header className='py-4'>
-      <nav className='dash-nav-container container d-flex justify-content-between'>
-        <div className='dash-nav-logo'>
-          <img src={logo} alt='logo' />
+    <header className="dash py-4 mb-5">
+      <nav className="dash-nav-container container d-xl-flex justify-content-between">
+        <div className="dash-nav-logo">
+          <img src={logo} alt="logo" />
         </div>
-        <div className='dash-nav-items d-flex align-items-center'>
+        <div className="dash-nav-items d-flex align-items-center">
           <h5>Courses</h5>
-          <Input className='mb-5' placeholder='Search' icon={<FaSearch />} />
-          <span id='dash-bell'>
+          <Input className="mb-5" placeholder="Search" icon={<FaSearch />} />
+          <span id="dash-bell">
             <FaRegBell />
           </span>
-          <div className='dash-user-icon d-flex'>
+          <div className="dash-user-icon d-flex">
             <img
               src={dashAvater}
-              alt='dash-avatar'
-              className='dashimage'
-              height='30'
-              width='30'
+              alt="dash-avatar"
+              className="dashimage"
+              height="30"
+              width="30"
             />
-            <span id='dash-toggle'>
+            <span id="dash-toggle">
               <RiArrowDownSLine />
             </span>
           </div>
