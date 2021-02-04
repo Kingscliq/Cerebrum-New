2d4603e2f696b5847823883dc74e33fc19a0df60
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2021-02-04 14:39:37 +0100

    fixing of the buy course page

--- src/pages/app/BuyCourse/index.js
+++ src/pages/app/BuyCourse/index.js
@@ -2,23 +2,46 @@
 
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { FaWindows } from "react-icons/fa";
 import { dummyVideoImage } from "../../../assets/images";
 import { DashboardHeader } from "../../../widgets/DashboardHeader";
 import { Footer } from "../../../widgets/Footer";
 
 const BuyCourse = () => {
-  const [course, setCourse] = useState();
-  //   const [courseId, setCourseId] = useState();
+  const [courses, setCourses] = useState([]);
+  const [course, setCourse] = useState({});
+  const [tutor, setTutor] = useState({});
+  const [courseId, setCourseId] = useState("");
 
+  const handleBuy = () => {
+    window.open(``);
+    console.log("Course Bought");
+  };
+  const watchCourse = () => {
+    window.open(`/watchcourse/${courseId}`, "_blank");
+  };
   useEffect(() => {
     const url_string = window.location.href;
     const url = new URL(url_string);
     const course_id = url.searchParams.get("id");
+    setCourseId(course_id);
     axios
-      .get(`https://cerebrum-v1.herokuapp.com/api/course/view/${course_id}`)
-      .then((res) => console.log(res.data))
+      .get(`https://cerebrum-v1.herokuapp.com/api/course/${course_id}`)
+      .then((res) => {
+        const data = res.data.data;
+        data.map((course) => {
+          setCourse(course);
+          //   console.log(course.tutor_id);
+          setTutor(course.tutor_id);
+          //   console.log(tutor.firstName);
+          //   map((tutor) => {
+          //     console.log(tutor.firstName);
+          //   });
+        });
+      })
       .catch((err) => console.log(err.response));
   }, []);
+  //
   return (
     <div>
       <DashboardHeader />
@@ -27,7 +50,7 @@ const BuyCourse = () => {
           <div className='row'>
             <div className='col-md-4'>
               <img
-                src={dummyVideoImage}
+                src={course.image_url}
                 alt='cerebrum dummy'
                 className='image-fluid w-100'
               />
@@ -39,25 +62,35 @@ const BuyCourse = () => {
                     className='text-bold mt-md-3'
                     style={{ fontWeight: "600" }}
                   >
-                    The Story of Art
+                    {course.name}
                   </h1>
-                  <small>By Lil Kim</small>
+                  <small>By {`${tutor.firstName} ${tutor.lastName}`}</small>
                 </div>
                 <div className='mt-4'>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Maxime sapiente distinctio totam culpa, odio quo repellat
-                    error dolor! Pariatur, reiciendis!
-                  </p>
+                  <p>{course.description}</p>
                 </div>
                 <div className='mt-5'>
-                  <small className='btn btn-warning text-light'>N16,000</small>
+                  <small className='btn btn-warning text-light'>{`N ${course.price}`}</small>
                 </div>
               </div>
             </div>
           </div>
           <div className='d-flex justify-content-end'>
-            <button className='btn btn-primary pull-right'>Buy Course</button>
+            {course.price > 0 ? (
+              <button
+                className='btn btn-primary pull-right'
+                onClick={handleBuy}
+              >
+                Buy Course
+              </button>
+            ) : (
+              <button
+                className='btn btn-primary pull-right'
+                onClick={watchCourse}
+              >
+                Watch Course
+              </button>
+            )}
           </div>
         </div>
       </div>
