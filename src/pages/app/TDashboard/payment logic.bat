d7005936527b9c67b7cd24522623baae79870670
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2021-02-08 12:05:52 +0100

    completion of the bycourse logic

---
+++ src/pages/app/TDashboard/payment logic.bat
@@ -0,0 +1,16 @@
+once the user clicks on Buy course the user is redirected to the subscription page
+    if the user is not logged in the user is redirected back to the login page and 
+    then the href stored on the local storage
+if the user is logged in once the user logs in, then 
+then href is gotten back from the localStorage and 
+then the user redirected to the buy course page
+
+
+if the user is logged in then the user is redirected subscription page, selects payment option
+
+the user clicks on the payment option, the paymentid and courseid is picked and stored in a state,
+
+Make a post Request to the APi to send the user and course details 
+
+
+    
